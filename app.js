6d7772cfca73851528e7f80e75a2c9518f1abfbd
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2022-10-24 09:25:55 -0500

    final commit JS Operators Exercise

---
+++ app.js
@@ -0,0 +1,47 @@
+//JS Operators
+//Excercise 1
+
+let a = 20,
+  b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let divide = a / b;
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+
+//Excercise 2
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(num + firstName);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
+//Excercise 3
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isAwake);
+console.log("eleven" == str4 && val > str3);
+console.log(!isAwake || isAwake);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
